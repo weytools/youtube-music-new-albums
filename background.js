5350e2cdaffdea2cbe1abd9a68f94537e38e38ec
wey <weytools@gmail.com>
2025-12-19 11:17:10 -0500

    Transformed my bookmarklet into Chrome extension to run on click

---
+++ background.js
@@ -0,0 +1,56 @@
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.action.setBadgeText({
+        text: "OFF",
+    });
+});
+
+
+// const newReleaseURL = 'https://music.youtube.com/new_releases/albums'
+
+// chrome.action.onClicked.addListener(async (tab) => {
+//     const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+//     const nextState = prevState === 'ON' ? 'OFF' : 'ON';
+//     // Set the action badge to the next state
+//     await chrome.action.setBadgeText({
+//         tabId: tab.id,
+//         text: nextState,
+//     });
+// })
+
+
+const newReleaseURL = 'https://music.youtube.com/new_releases/albums'
+
+chrome.action.onClicked.addListener(async (tab) => {
+    if (tab.url.startsWith(newReleaseURL)) {
+        // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
+        const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+        // Next state will always be the opposite
+        const nextState = prevState === 'ON' ? 'OFF' : 'ON';
+
+        // Set the action badge to the next state
+        await chrome.action.setBadgeText({
+            tabId: tab.id,
+            text: nextState,
+        });
+
+        if (nextState === "ON") {
+            // Insert the CSS file when the user turns the extension on
+            await chrome.scripting.insertCSS({
+                files: ["new-releases.css"],
+                target: { tabId: tab.id },
+            });
+            chrome.scripting
+                .executeScript({
+                    target: { tabId: tab.id },
+                    files: ["run-highlighter.js"],
+                })
+                .then(() => console.log("script injected"));
+        } else if (nextState === "OFF") {
+            // Remove the CSS file when the user turns the extension off
+            await chrome.scripting.removeCSS({
+                files: ["new-releases.css"],
+                target: { tabId: tab.id },
+            });
+        }
+    }
+});
---
+++ original-bookmarklet.js
@@ -0,0 +1,25 @@
+(function () {
+    let artistList = []
+    document.querySelectorAll('ytmusic-two-row-item-renderer').forEach(node => {
+        let isAlbum = node.querySelector('span.yt-formatted-string').textContent == 'Album'
+        if (isAlbum) {
+            node.style.background = 'green'
+            // get artist
+            let artistName = ''
+            node.querySelectorAll('.subtitle .yt-formatted-string.style-scope').forEach((s, i) => {
+                if (i != 0) {
+                    if (i != 1) {
+                        artistName += s.textContent
+                    }
+                }
+            })
+            artistList.push(artistName)
+        }
+    })
+    let headerTag = document.querySelector("h1")
+    let listParagraph = document.createElement("p")
+    listParagraph.textContent = artistList.join('\n')
+    listParagraph.style.whiteSpace = 'pre-wrap';
+    listParagraph.style.columnCount = 3;
+    headerTag.after(listParagraph);
+})();
